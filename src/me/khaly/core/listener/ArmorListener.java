dd95cc5f498db7723760c783e88898bc6061d182
iAtog <atogmc58vani@gmail.com>
2021-10-19 22:17:27 -0400

    recoded all core

--- src/me/khaly/core/listener/ArmorListener.java
+++ src/me/khaly/core/listener/ArmorListener.java
@@ -33,15 +33,19 @@ public class ArmorListener implements Listener{
 	
 	@EventHandler(priority =  EventPriority.HIGHEST, ignoreCancelled = true)
 	public final void inventoryClick(final InventoryClickEvent e){
-		boolean shift = false, numberkey = false;
-		if(e.isCancelled()) return;
-		if(e.getAction() == InventoryAction.NOTHING) return; // Why does this get called if nothing happens??
+		boolean shift = false;
+		boolean numberkey = false;
+		if(e.isCancelled() || e.getAction() == InventoryAction.NOTHING) {
+			return; // Why does this get called if nothing happens??
+		}
 		if(e.getClick() == ClickType.SHIFT_LEFT || e.getClick() == ClickType.SHIFT_RIGHT){
 			shift = true;
 		}
-		if(e.getClick() == ClickType.NUMBER_KEY){
+		
+		if(e.getClick() == ClickType.NUMBER_KEY) {
 			numberkey = true;
 		}
+		
 		if(e.getSlotType() != SlotType.ARMOR && e.getSlotType() != SlotType.QUICKBAR && e.getSlotType() != SlotType.CONTAINER) return;
 		if(e.getClickedInventory() != null && e.getClickedInventory().getType() != InventoryType.PLAYER) return;
 		if(e.getInventory().getType() != InventoryType.CRAFTING && e.getInventory().getType() != InventoryType.PLAYER) return;
